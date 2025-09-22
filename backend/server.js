0640ff247b1c3c2e0a87753e83e29c3ec54a2556
Sunny Jain <jainsunny0308@gmail.com>
2025-09-22 16:55:07 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     process.env.FRONTEND_URL,
+    "https://www.mindymunchs.com/"
   ],
   credentials: true,
   optionsSuccessStatus: 200,
